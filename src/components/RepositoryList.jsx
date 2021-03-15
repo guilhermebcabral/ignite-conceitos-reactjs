cfbd1a7a04103c33f79715991652902e86c7cfee
Guilherme B Cabral <guibcabral19@gmail.com>
2021-03-15 18:55:34 -0300

    get api data with useEffect

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,6 +1,9 @@
+import { useState, useEffect } from 'react'
 import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss'
+
+// https://api.github.com/orgs/rocketseat/repos
 
 const repository = {
   name: 'Unform',
@@ -11,6 +14,14 @@ const repository = {
 
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    fetch('https://api.github.com/orgs/rocketseat/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data))
+  }, [])
+
   return (
     <section className="repository-list">
       <h1>
