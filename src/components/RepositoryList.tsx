c1e5b22564b47f39d012d2f3b14c4ac7ca789c62
Guilherme B Cabral <guibcabral19@gmail.com>
2021-03-16 21:32:59 -0300

    typescript components

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -3,8 +3,14 @@ import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss'
 
+interface Repository {
+  name: string
+  description: string
+  html_url: string
+}
+
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([])
+  const [repositories, setRepositories] = useState<Repository[]>([])
 
   useEffect(() => {
     fetch('https://api.github.com/orgs/rocketseat/repos')
@@ -14,9 +20,7 @@ export function RepositoryList() {
 
   return (
     <section className="repository-list">
-      <h1>
-        Lista de Repositórios
-      </h1>
+      <h1> Lista de Repositórios </h1>
 
       <ul>
         {repositories.map(repository => {
